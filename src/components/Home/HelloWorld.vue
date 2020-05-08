1182e8e87aebebb41669b39e82af52d29625d0c5
MohamedBeydoun <mohamed.beydoun@mail.mcgill.ca>
2020-05-08 13:12:18 -0400

    Cleaned home page

--- src/components/Home/HelloWorld.vue
+++ src/components/Home/HelloWorld.vue
@@ -1,6 +1,10 @@
 <template>
   <v-container>
-    <h1>Hello World</h1>
+    <div class="dashbaord">
+      <v-container class="my-5">
+        <h1 class="my-5 display-1 black--text text-uppercase">hello world</h1>
+      </v-container>
+    </div>
   </v-container>
 </template>
 
