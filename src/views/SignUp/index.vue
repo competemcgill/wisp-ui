89d41817b11c74b3540f4d7aa08cd25c76abe11c
Ahmed Elehwany <ahmed.elehwany@mail.mcgill.ca>
2020-05-15 04:34:42 -0400

    Change signup view

--- src/views/SignUp/index.vue
+++ src/views/SignUp/index.vue
@@ -2,85 +2,85 @@
 <template>
   <div class="signup">
     <v-container class="my-5">
-      <v-layout row>
-        <v-flex xs12 sm6 offset-sm3>
-          <v-card>
+      <h1 class="my-5 display-1 black--text text-uppercase text-xs-center">
+        Register
+      </h1>
+      <v-row class="my-5 py-5" justify="center" align="center">
+        <v-col cols="12" sm="6" class="my-5 py-5">
+          <v-card >
             <v-card-text>
-              <v-container>
-                <form>
-                  <v-row justify="center" align="center">
-                    <h1 class="my-5 display-1 black--text text-uppercase">Register</h1>
-                  </v-row>
+              <v-container >
                   <v-text-field
+                    class="mb-3"
                     name="username"
                     label="Username"
                     id="username"
                     v-model="user.username"
                     type="username"
-                    outlined
+                    prepend-icon="mdi-account"
                     required>
                   </v-text-field>
                   <v-text-field
+                    class="mb-3"
                     name="password"
                     label="Password"
                     id="password"
                     v-model="user.password"
                     type="password"
-                    outlined
+                    prepend-icon="mdi-key"
                     required>
                   </v-text-field>
                   <v-text-field
+                    class="mb-3"
                     name="confirmPassword"
                     label="Confirm Password"
                     id="confirmPassword"
                     :rules="passwordRules"
                     type="password"
-                    outlined
                     :disabled="user.password.length<=0"
+                    prepend-icon="mdi-key"
                     required>
                   </v-text-field>
                   <v-text-field
+                    class="mb-3"
                     name="email"
                     label="E-mail address"
                     id="email"
                     v-model="user.email"
                     :rules="emailRules"
                     type="email"
-                    outlined
+                    prepend-icon="mdi-email"
                     required>
                   </v-text-field>
                   <v-text-field
+                    class="mb-3"
                     name="codeforcesUsername"
                     label="Codeforces Username"
                     id="codeforcesUsername"
                     v-model="user.codeforcesUsername"
                     type="username"
-                    outlined
+                    prepend-icon="mdi-poll"
                     required>
                   </v-text-field>
-                  <v-row justify="center" align="center" >
-                    <v-col cols="12" >
-                      <v-btn
-                        block
-                        @click="signup"
-                        class="offset-y primary--text"
-                        type="submit"
-                      >Sign up
-                      </v-btn>
-                    </v-col>
-                  </v-row>
-                </form>
+                  <v-btn
+                    block
+                    @click="signup"
+                    class="offset-y primary--text"
+                    type="submit"
+                  >Sign up
+                  </v-btn>
               </v-container>
             </v-card-text>
           </v-card>
-        </v-flex>
-      </v-layout>
+        </v-col>
+      </v-row>
     </v-container>
   </div>
 </template>
 
 <script>
   import { api } from "@/gateways/wisp-api";
+  import { eventBus } from "@/store/eventBus";
 
   export default {
     name: "SignUp",
@@ -128,9 +128,9 @@
             password: this.user.password
           });
 
-          console.log("hello");
           this.$store.dispatch("setToken", data.token);
-          this.$store.dispatch("setUser", data);
+          this.$store.dispatch("setUser", data.user);
+          eventBus.$emit("LOGIN_SUCCESS");
           this.$router.push("/dashboard");
         } catch (err) {
           this.error = err.response.data.message;
