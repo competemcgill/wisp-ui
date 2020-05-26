91a7bfa065a0d298ddc7dd07cb91b8fd52cb18e5
Mohamed Beydoun <41027939+MohamedBeydoun@users.noreply.github.com>
2020-05-26 17:13:26 -0400

    Implemented caching with webpack (#62)
    * Added nginx conf to remove caching
    
    * Updated caching rules to only include css and js files
    
    * Changed caching strategy to work from webpack
    
    * Added timestamp to html to verify builds

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
+  configureWebpack: config => {
+    config.output.filename = `[name].${Date.now().valueOf()}.[chunkhash].js`;
+  },
   devServer: {
     watchOptions: {
       poll: 500
