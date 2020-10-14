e9321408d146fbb1f6d6975bd75151df8d856dd7
Valerian Clerc <valerian.clerc@mail.mcgill.ca>
2020-10-14 17:16:32 -0400

    Update home page (#91)
    * Fix for user data not reloading on refresh (#60) (#61)
    
    * Update Home page message from summer training to year-round memo
    
    Co-authored-by: Mohamed Beydoun <41027939+MohamedBeydoun@users.noreply.github.com>

--- src/components/Home/Announcement.vue
+++ src/components/Home/Announcement.vue
@@ -5,137 +5,27 @@
       <v-container>
         <v-row>
           <v-card-title
-            >Welcome to Compete McGill's Summer Training</v-card-title
+            >Welcome to Compete McGill's Training Platform</v-card-title
           >
-          <v-card-text class="grey--text">Fri, May 15 2020</v-card-text>
-          <v-card-text class="social-link-icons">
-            <v-col lg="2" md="4">
-              <v-row justify="space-between">
-                <v-btn icon>
-                  <v-icon
-                    medium
-                    class="primary--text"
-                    @click="openLink('discord')"
-                    >mdi-discord</v-icon
-                  >
-                </v-btn>
-                <v-btn icon>
-                  <v-icon
-                    medium
-                    class="primary--text"
-                    @click="openLink('facebook')"
-                    >mdi-facebook</v-icon
-                  >
-                </v-btn>
-                <v-btn icon>
-                  <v-icon medium class="primary--text" @click="openLink('form')"
-                    >mdi-clipboard-text-multiple</v-icon
-                  >
-                </v-btn>
-              </v-row>
-            </v-col>
-          </v-card-text>
+          <v-card-text class="grey--text">Wed, Oct 14 2020</v-card-text>
           <v-card-title>What's this all about?</v-card-title>
           <v-card-text>
             Competitive Programming sounds cool (okay, that's debatable), but
             it's one of those things that can be pretty daunting to start,
-            particularly if you don't have any kind of guidance. That's why,
-            this summer, while we're all quarantining, we're excited to invite
-            you to take part in Compete McGill's Summer Training 2.0!
-            <br />
-            <br />Participants will take part in a weekly reading group that
-            discusses the mother of all Competitive Programming textbooks:
-            <a href="https://cpbook.net/" target="_blank"
-              >Competitive Programming 3</a
-            >
-            by Steven Halim. By the end of the summer, you'll have a decent
-            understanding of all of the core algorithms needed for Competitive
-            Programming, and a good amount of practice, especially coding them
-            in Java and/or C++.
+            particularly if you don't have any kind of guidance. That's why
+            we’ve built our very own training platform for you to follow along
+            and get the practice you want. Whether you are participating in our
+            summer training program, trying out the selected workshop problems
+            or just want some practice WISP is here for you. After all WISP Is
+            for Solving Problems 😉
           </v-card-text>
-          <v-card-title>Bi-Weekly Plan</v-card-title>
           <v-card-text>
-            <ul>
-              <li>
-                <b>Monday (bi-weekly):</b> Problem Set and Readings are
-                released.
-              </li>
-              <li>
-                <b>Saturday (bi-weekly):</b> Discussion session takes place on
-                Discord from 2-3PM EST. Use this time to discuss any issues
-                you're having with our workshop leads who are all World-Class
-                Competitive Programmers.
-              </li>
-              <li>
-                <b>At the end of every topic (bi-weekly)</b>, we'll host a
-                friendly competition! This will give you some experience coding
-                in a time-constrained, (moderately) competitive enviornment! Use
-                this time to focus on improving!
-              </li>
-            </ul>
-          </v-card-text>
-          <v-card-title>Schedule</v-card-title>
-          <v-card-text>
-            <v-simple-table fixed-header height="300px">
-              <thead>
-                <tr>
-                  <th class="text-left">Date</th>
-                  <th class="text-left">Topics</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>May 18th</td>
-                  <td>
-                    <ul>
-                      <li>Introduction (Basic C++ Concepts)</li>
-                      <li>What is Competitive Programming?</li>
-                      <li>What does CP code look like?</li>
-                      <li>Input/Output</li>
-                      <li>Basic Data Structures</li>
-                    </ul>
-                  </td>
-                </tr>
-                <tr>
-                  <td>June 1st</td>
-                  <td>
-                    Data Structures:
-                    <ul>
-                      <li>Sets</li>
-                      <li>Maps</li>
-                      <li>Vectors</li>
-                      <li>Range Queries</li>
-                    </ul>
-                  </td>
-                </tr>
-                <tr>
-                  <td>June 15th</td>
-                  <td>Graph Algorithms</td>
-                </tr>
-                <tr>
-                  <td>June 29th</td>
-                  <td>
-                    <ul>
-                      <li>Sorting</li>
-                      <li>Searching</li>
-                      <li>Greedy Algorithms</li>
-                    </ul>
-                  </td>
-                </tr>
-                <tr>
-                  <td>July 13th</td>
-                  <td>Dynamic Programming</td>
-                </tr>
-                <tr>
-                  <td>July 27th</td>
-                  <td>Math</td>
-                </tr>
-                <tr>
-                  <td>August 10th</td>
-                  <td>String Algorithms</td>
-                </tr>
-              </tbody>
-            </v-simple-table>
+            Check out
+            <a href="https://compete-mcgill.ca/">our website</a> for more info
+            about our club, or
+            <a href="https://www.facebook.com/CompeteMcGill">
+              our Facebook page </a
+            >for upcoming events!
           </v-card-text>
         </v-row>
       </v-container>
